0f86f68761bdc763285074b1dc0c700ec321b2c5
Daniel <dandcalvo@gmail.com>
2024-07-16 16:59:45 -0700

    notes

---
+++ index.js
@@ -0,0 +1,13 @@
+// Create a 2D array that represents the board
+// adjacency list for each cell in the node
+//  generate all possible moves from a starting cell
+//    +1 +2
+//    +2 +1
+//    +2 -1
+//    +1 -2
+//    -1 -2
+//    -2 -1
+//    
+//    
+//    
+//    
